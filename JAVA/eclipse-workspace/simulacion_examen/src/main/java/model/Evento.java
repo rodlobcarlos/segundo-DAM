6c36683afb402e8e88cd0aac4d9db7879961b792
rodlobcarlos <rodlobcarlos@gmail.com>
2025-09-22 21:29:28 +0200

    clases

--- JAVA/eclipse-workspace/simulacion_examen/src/main/java/model/Evento.java
+++ JAVA/eclipse-workspace/simulacion_examen/src/main/java/model/Evento.java
@@ -1,6 +1,7 @@
 package model;
 
 import java.time.LocalDate;
+import java.util.Objects;
 
 public class Evento {
 
@@ -8,5 +9,124 @@ public class Evento {
 	private int id;
 	private String nombre;
 	private LocalDate fecha;
-	private int num_entradas;
+	private int num_entradasVendidas;
+	private int capacidad_maxima_asistentes;
+	private EstadoEven estado;
+
+	public static int getContador() {
+		return contador;
+	}
+
+	public static void setContador(int contador) {
+		Evento.contador = contador;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public LocalDate getFecha() {
+		return fecha;
+	}
+
+	public void setFecha(LocalDate fecha) {
+		this.fecha = fecha;
+	}
+
+	public int getNum_entradasVendidas() {
+		return num_entradasVendidas;
+	}
+
+	public void setNum_entradasVendidas(int num_entradasVendidas) {
+		this.num_entradasVendidas = num_entradasVendidas;
+	}
+
+	public int getCapacidad_maxima_asistentes() {
+		return capacidad_maxima_asistentes;
+	}
+
+	public void setCapacidad_maxima_asistentes(int capacidad_maxima_asistentes) {
+		this.capacidad_maxima_asistentes = capacidad_maxima_asistentes;
+	}
+
+	public EstadoEven getEstado() {
+		return estado;
+	}
+
+	public void setEstado(EstadoEven estado) {
+		this.estado = estado;
+	}
+
+	public Evento(int id, String nombre, LocalDate fecha, int num_entradasVendidas, int capacidad_maxima_asistentes,
+			EstadoEven estado) {
+		super();
+		this.id = id;
+		this.nombre = nombre;
+		this.fecha = fecha;
+		this.num_entradasVendidas = num_entradasVendidas;
+		this.capacidad_maxima_asistentes = capacidad_maxima_asistentes;
+		this.estado = estado;
+	}
+
+	@Override
+	public String toString() {
+		return "Evento [nombre=" + nombre + ", fecha=" + fecha + ", estado=" + estado + "]";
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Evento other = (Evento) obj;
+		return id == other.id;
+	}
+
+	public boolean validarDatos(Evento e) throws CraftterException {
+		boolean valido = false;
+		if (e.capacidad_maxima_asistentes < 0 || e.num_entradasVendidas < 0) {
+			valido = true;
+		} else if (e.num_entradasVendidas > e.capacidad_maxima_asistentes) {
+			valido = true;
+		} else {
+			throw new CraftterException("No es posible contruir un evento con esos datos.");
+		}
+		return valido;
+
+	}
+
+	EstadoEven even;
+
+	public boolean modificaEvento(EstadoEven estado) {
+		boolean EstadoCorrecto = false;
+		if(estado.equals(estado.APLAZADO) && even.equals(even.PROGRAMADO)) {
+			EstadoCorrecto = true;
+			EstadoEven evento = EstadoEven.PROGRAMADO;
+			System.out.println("El estado se ha cambiado a: " + evento);
+		} else if(estado.equals(estado.PENDIENTE) && even.equals(even.PROGRAMADO) {
+			 
+		}
+		return EstadoCorrecto;
+		
+	}
 }
