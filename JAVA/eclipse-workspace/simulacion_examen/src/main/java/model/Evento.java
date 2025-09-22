2aeedbf32b99214badc8b46fb78575e48a23939f
Carlos Rodríguez Lobato <rodlobcarlos@gmail.com>
2025-09-22 11:38:35 +0200

    repaso

---
+++ JAVA/eclipse-workspace/simulacion_examen/src/main/java/model/Evento.java
@@ -0,0 +1,12 @@
+package model;
+
+import java.time.LocalDate;
+
+public class Evento {
+
+	private static int contador;
+	private int id;
+	private String nombre;
+	private LocalDate fecha;
+	private int num_entradas;
+}
