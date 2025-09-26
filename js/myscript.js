d0674068106f022f842f2e76e99b01a65a8c648d
Carlos Rodríguez Lobato <rodlobcarlos@gmail.com>
2025-09-26 11:54:02 +0200

    first commit

---
+++ js/myscript.js
@@ -0,0 +1,14 @@
+alert("Hello to JS");
+console.log("Hola buenos dias!");
+
+var table = "Normal Table";
+let chair = "One chair";
+
+let testBoolean = true;
+console.log(testBoolean);
+
+let testString = "text";
+console.log(testString);
+
+let testnumber = 10;
+console.log(testnumber);
